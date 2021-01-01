74df257b69819cdcf39c28ac6040594a3cf1dea2
Avnichopra121 <69035757+Avnichopra121@users.noreply.github.com>
2021-01-01 19:56:31 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,23 @@
+class Tree 
+{
+    constructor(x,y,width,height) 
+    {
+      var options = 
+      {
+          isStatic: false
+      }
+
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("tree.png")
+      World.add(world, this.body);
+    }
+
+    display()
+    {
+      var pos = this.body.position;
+      imageMode(CENTER);
+      image(this.image,800,250,this.width,this.height);
+    }
+  }
